4e118562c69ca5b0f9895305176991b154935fb6
leekyuwon <kyuwon.lee@makinarocks.ai>
2023-11-02 09:37:12 +0900

    Initial commit

---
+++ src/test/java/com/study/enterprise/EnterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.enterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
